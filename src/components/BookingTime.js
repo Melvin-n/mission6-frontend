ae865b3dca7544edb1a6399226534abc88d31216
Ana <may_piccio@yahoo.com>
2022-03-21 21:54:30 +1300

    add BookingTime.js component

--- src/components/BookingTime.js
+++ src/components/BookingTime.js
@@ -14,19 +14,27 @@ function Example() {
   
     return (
       <div ref={ref}>
-        <Button onClick={handleClick}>Holy guacamole!</Button>
+        <Button className="btn-md" variant="primary" onClick={handleClick}>Icon</Button>
   
         <Overlay
           show={show}
           target={target}
           placement="bottom"
           container={ref}
-          containerPadding={20}
-        >
+          containerPadding={20}>
           <Popover id="popover-contained">
-            <Popover.Header as="h3">Popover bottom</Popover.Header>
+            <Popover.Header as="h3">Morning</Popover.Header>
             <Popover.Body>
-              <strong>Holy guacamole!</strong> Check this info.
+              {/* <strong>Morning</strong> Check this info. */}
+              9:00   10:00   11:00
+            </Popover.Body>
+            <Popover.Header as="h3">Afternoon</Popover.Header>
+            <Popover.Body>
+              1:00   2:00   3:00 
+            </Popover.Body>
+            <Popover.Header as="h3">Evening</Popover.Header>
+            <Popover.Body>
+              5:00
             </Popover.Body>
           </Popover>
         </Overlay>
