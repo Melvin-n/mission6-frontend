c79f860684e5609149e90bfcda662e73168ca61c
MitchellQ <mitchell.quarrie@outlook.com>
2022-03-23 15:59:48 +1300

    Finished work on left side of Property Card

--- src/components/PropertyView/PropertyCard.js
+++ src/components/PropertyView/PropertyCard.js
@@ -13,10 +13,18 @@ const PropertyCard = ({ Property, List }) => {
                     <h3 className='address'>{Property.Property.address}</h3>
                     <h4 className='price'>{`$${Property.Property.pricePerWeek}/wk`}</h4>
                 </div>
-                <div className='left-middle'></div>
+                <div className='left-middle'>
+                    <p className="manager">
+                        <b> {Property.Property.propertyManager}</b> <br />
+                        Property Manager
+                    </p>
+                    <p className="contact">
+                        {`Email: ${Property.Property.propertyManagerEmail}`} <br />
+                        {`Phone: ${Property.Property.propertyManagerPhone}`}
+                    </p>
+                </div>
 
                 <Map address={Property.Property.address} />
-
             </div>
             <div className='card-right'>
 
