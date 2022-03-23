d19a420cbce90d67bd6de32dc6d60dc1585fcabc
Ana <may_piccio@yahoo.com>
2022-03-23 19:13:19 +1300

    install axios, setup newUser properties in Booking.js

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,9 +1,9 @@
-import React, {useState} from "react";
+import {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
-
+// import axios from "axios";
 import "./Booking.css";
 
 import Button from 'react-bootstrap/Button';
@@ -13,10 +13,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
 
-const Booking = () => {
+const Booking = (prop) => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [phoneNum, setPhoneNum] = useState("");
@@ -24,6 +22,8 @@ const Booking = () => {
   const [sizeViewGroup, setSizeViewGroup] = useState("");
   const [viewDate, setViewDate] = useState("");
   const [viewTime, setViewTime] = useState("");
+
+  const navigate = useNavigate();
 
   const reset = () => {
     setFirstName("");
@@ -37,13 +37,15 @@ const Booking = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    // console.log(firstName, lastName, phoneNum, email, sizeViewGroup, viewDate, viewTime );
+    // navigate('/confirm');
     createUser();
   };
 
-  const createUser = () => {
+  const createUser = async() => {
     const baseURL = "http://localhost:5000/api/users";
 
-   const newUser = {
+    const newUser = {
      firstName,
      lastName,
      phoneNum,
@@ -53,12 +55,12 @@ const Booking = () => {
      viewTime
    };
    console.log(newUser);
-   try {
+  //  try {
       // const response = await axios.post(baseURL, newUser);
       // console.log(response.data);
-   } catch (err) {
-      console.log(err);
-   }
+  //  } catch (err) {
+      // console.log(err);
+  //  }
   };
 
   return (
@@ -91,82 +93,98 @@ const Booking = () => {
               </Card>
             </Col>
             <Col sm={7}>
-              <Container>
-                <p>Viewing date and time</p>
-                <Row>
-                  <Col sm={6}>
-                    <Form.Control type="date" name="" />
-                  </Col>
-
-                  <Col sm={6}>
-                  <Col className="my-0 mb-3">
-                      <Form.Control type="time" placeholder="Last name" />
-                    </Col>
-
-                  </Col>
-                </Row>
-                <p className="mt-3">Your information</p>
                 <Form>
-                  <Row>
+                  <Form.Text className="mb-4 pb-5 view-date-time">Viewing date and time</Form.Text>
+                 <Row>
+                  <Col sm={6}>
+                    <Form.Control 
+                      type="date" 
+                      onChange={(e) => setViewDate(e.target.value)}
+                      value={viewDate}
+                    />
+                  </Col>
+                  <Col sm={6}>
                     <Col className="my-0 mb-3">
-                      <Form.Control placeholder="First name" />
+                      <Form.Control 
+                        type="time"
+                        onChange={(e) => setViewTime(e.target.value)}
+                        value={viewTime} />
+                    </Col>
+                  </Col>
+                 </Row>
+                  <Form.Text className="mb-4 pb-5 yourinfo">Your information</Form.Text>
+                   <Row>
+                    <Col className="my-0 mb-3">
+                      <Form.Control 
+                        type="text" 
+                        placeholder="First name" 
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName} /> 
                     </Col>
                     <Col className="my-0 mb-3">
-                      <Form.Control placeholder="Last name" />
+                      <Form.Control 
+                        type="text" 
+                        placeholder="Last name"
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName} />
                     </Col>
                   </Row>
                   <Row>
                     <Col className="my-2">
-                      <Form.Control placeholder="Phone number" />
+                      <Form.Control 
+                        type="text" 
+                        placeholder="Phone number"
+                        onChange={(e) => setPhoneNum(e.target.value)}
+                        value={phoneNum} />
                     </Col>
                     <Col className="my-2">
                       <Form.Group as={Col} controlId="formGridEmail">
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control 
+                          type="email" 
+                          placeholder="Enter email"
+                          onChange={(e) => setEmail(e.target.value)} 
+                          value={email} />
                       </Form.Group>
                     </Col>
                   </Row>
                   <Row>
                     <Col className="my-2">
-                      <p>Group information</p>
-                      <DropdownButton
-                        className="pl-0"
-                        id="dropdown-basic-button"
-                        variant="light"
-                        title="Size of viewing group"
-                      >
-                        <Dropdown.Item href="#/action-1">1</Dropdown.Item>
-                        <Dropdown.Item href="#/action-2">2</Dropdown.Item>
-                        <Dropdown.Item href="#/action-3">3</Dropdown.Item>
-                      </DropdownButton>
+                       <Form.Text className="mb-4 pb-5 group-info ">Group information</Form.Text>
+                        <Form.Select 
+                          aria-label="Default select example"
+                          onChange={(e) => setSizeViewGroup(e.target.value)}
+                          value={sizeViewGroup}>
+                          <option>Size of view group</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </Form.Select>
                     </Col>
                     <Col className="my-2">
-                      <p>Agree to Covid-19 regulation</p>
-                      <form>
-                        <label className="checkbox-margin my-2">
-                          Read regulation
-                        </label>
-                        <input type="checkbox" className="checkbox-color" />
-                      </form>
+                       <Form.Text className="mb-4 pb-5 agree-covid" >Agree to Covid-19 regulation</Form.Text>
+                       <Form.Check 
+                         className="checkbox-color ml-5" 
+                         type="checkbox"
+                        />
                     </Col>
                   </Row>
                   <Row>
                     <Col sm={6}>
-                      <p>Preferred move in date</p>
-                      <Form.Control type="date" name="" />
+                       <Form.Text className="mb-4 pb-5 movein-date" >Preferred move in date</Form.Text>
+                       <Form.Control type="date" />
                     </Col>
                   </Row>
+                  <Row>
+                    <Col className="text-center">
+                    {/* <Link to="/confirm"> */}
+                     <Button className="mt-3" variant="danger" onClick={handleSubmit}>
+                        Book a viewing
+                     </Button>
+                    {/* </Link> */}
+                   </Col>
+                  </Row>
                 </Form>
-              </Container>
             </Col>
-          </Row>
-          <Row>
-            <Col className="text-center">
-          <Link to="/confirm">
-          <Button className="mt-3" variant="danger">
-           Book a viewing
-          </Button>
-        </Link>
-          </Col>
           </Row>
         </Container>
       </Container>
--- src/pages/Confirm.js
+++ src/pages/Confirm.js
@@ -1,112 +1,196 @@
-import React from "react";
+import {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
-import "./Confirm.css";
+// import axios from "axios";
+import "./Booking.css";
 
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Image from "react-bootstrap/Image";
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
 
-export default function Confirm(props) {
+const Booking = (prop) => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+  const [email, setEmail] = useState("");
+  const [sizeViewGroup, setSizeViewGroup] = useState("");
+  const [viewDate, setViewDate] = useState("");
+  const [viewTime, setViewTime] = useState("");
+
+  const navigate = useNavigate();
+
+  const reset = () => {
+    setFirstName("");
+    setLastName("");
+    setPhoneNum("");
+    setEmail("");
+    setSizeViewGroup("");
+    setViewDate("");
+    setViewTime("");
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.log(firstName, lastName, phoneNum, email, sizeViewGroup, viewDate, viewTime );
+    // navigate('/confirm');
+    createUser();
+  };
+
+  const createUser = async() => {
+    const baseURL = "http://localhost:5000/api/users";
+
+    const newUser = {
+     firstName,
+     lastName,
+     phoneNum,
+     email,
+     sizeViewGroup,
+     viewDate,
+     viewTime
+   };
+   console.log(newUser);
+  //  try {
+      // const response = await axios.post(baseURL, newUser);
+      // console.log(response.data);
+  //  } catch (err) {
+      // console.log(err);
+  //  }
+  };
+
   return (
     <>
       <Header />
-      <Container className="mt-5">
-       <Container className="mx-6">
-        <Row className="">
-          <Col sm={5}>
-            <Card className="mt-0 ms-2">
-              <Card.Title className="card-title mt-0">
-               Thanks Peter, see you there!
-              </Card.Title>
-              <Row>
-               <Col>
-                <Image
-                  src="http://localhost:3000/images/Book-Viewing/Property card.png"
-                  fluid
-                  rounded
-                  className="my-0 px-1"
-                />
-               </Col>
-              </Row>
-              <Row>
-               <Col>
-                <Image
-                  src="http://localhost:3000/images/Book-Viewing/box/small.png"
-                  fluid
-                  rounded
-                  className="mb-4 pe-4"
-                />
-               </Col>
-              </Row>
-            </Card>
-          </Col>
-           <Col sm={7}>
-             <Row classname="ms-3">
-              <Col classname="">
-              <p>We look forward to seeing you at your viewing</p>
-              </Col>
-             </Row> 
-               <Row>
-                <Col sm={6}>
-                  <p>Viewing date and time</p>
-                  <p>
-                   `${props.time} + ${props.date}`
-                  </p>
-                </Col>
-                  <Col sm={6}>
-                  <p>Name</p>
-                  <p>`${props.name} + name`</p>
-                </Col>
-               </Row>
-              <Row>
-                <Col sm={6}>
-                  <p>Size of viewing group</p>
-                  <p>`${props.sizeGroup} + sizeGroup`</p>
-                </Col>
-                <Col sm={6}>
-                  <p>Email</p>
-                  <p>`${props.email} + email;`</p>
-                </Col>
-              </Row>
+      <Container className="container-main mt-5">
+        <Container className="container-2nd mx-5">
+          <Row>
+            <Col sm={5}>
+              <Card className="mt-2">
+                <Card.Title className="card-title mt-2">
+                  Thanks Peter, see you there!
+                </Card.Title>
                 <Row>
-                 <Col>
-                  <p className="add-to-calendar text-danger text-end my-0 py-0">
-                   Add to calendar
-                  </p>
-                 </Col> 
+                  <Image
+                    src="http://localhost:3000/images/Book-Viewing/Property card.png"
+                    fluid
+                    rounded
+                    className="my-0 px-1"
+                  />
                 </Row>
-                  <Row>
-                    <Col>
-                      <Image
-                        src="http://localhost:3000/images/Confirmation/image 1.png"
-                        fluid
-                        rounded
-                        className="my-0"
-                      />
+                <Row>
+                  <Image
+                    src="http://localhost:3000/images/Book-Viewing/box/small.png"
+                    fluid
+                    rounded
+                    className="mb-4 pe-4"
+                  />
+                </Row>
+              </Card>
+            </Col>
+            <Col sm={7}>
+              
+
+                {/* <Form>
+                  <Form.Text className="mb-4 pb-5 view-date-time">Viewing date and time</Form.Text>
+                 <Row>
+                  <Col sm={6}>
+                    <Form.Control 
+                      type="date" 
+                      onChange={(e) => setViewDate(e.target.value)}
+                      value={viewDate}
+                    />
+                  </Col>
+                  <Col sm={6}>
+                    <Col className="my-0 mb-3">
+                      <Form.Control 
+                        type="time"
+                        onChange={(e) => setViewTime(e.target.value)}
+                        value={viewTime} />
+                    </Col>
+                  </Col>
+                 </Row>
+                  <Form.Text className="mb-4 pb-5 yourinfo">Your information</Form.Text>
+                   <Row>
+                    <Col className="my-0 mb-3">
+                      <Form.Control 
+                        type="text" 
+                        placeholder="First name" 
+                        onChange={(e) => setFirstName(e.target.value)}
+                        value={firstName} /> 
+                    </Col>
+                    <Col className="my-0 mb-3">
+                      <Form.Control 
+                        type="text" 
+                        placeholder="Last name"
+                        onChange={(e) => setLastName(e.target.value)}
+                        value={lastName} />
                     </Col>
                   </Row>
                   <Row>
-                   <Col sm={6} className="">
-                    <Button className="mr-0" variant="outline-danger">
-                      Change booking details
-                    </Button>
-                   </Col>
-                   <Col sm={6} className="">
-                    <Button  variant="danger">
-                      Change booking details
-                    </Button>
+                    <Col className="my-2">
+                      <Form.Control 
+                        type="text" 
+                        placeholder="Phone number"
+                        onChange={(e) => setPhoneNum(e.target.value)}
+                        value={phoneNum} />
+                    </Col>
+                    <Col className="my-2">
+                      <Form.Group as={Col} controlId="formGridEmail">
+                        <Form.Control 
+                          type="email" 
+                          placeholder="Enter email"
+                          onChange={(e) => setEmail(e.target.value)} 
+                          value={email} />
+                      </Form.Group>
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="my-2">
+                       <Form.Text className="mb-4 pb-5 group-info ">Group information</Form.Text>
+                        <Form.Select 
+                          aria-label="Default select example"
+                          onChange={(e) => setSizeViewGroup(e.target.value)}
+                          value={sizeViewGroup}>
+                          <option>Size of view group</option>
+                          <option value="1">One</option>
+                          <option value="2">Two</option>
+                          <option value="3">Three</option>
+                        </Form.Select>
+                    </Col>
+                    <Col className="my-2">
+                       <Form.Text className="mb-4 pb-5 agree-covid" >Agree to Covid-19 regulation</Form.Text>
+                       <Form.Check 
+                         className="checkbox-color ml-5" 
+                         type="checkbox"
+                        />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col sm={6}>
+                       <Form.Text className="mb-4 pb-5 movein-date" >Preferred move in date</Form.Text>
+                       <Form.Control type="date" />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col className="text-center">
+                     <Button className="mt-3" variant="danger" onClick={handleSubmit}>
+                        Book a viewing
+                     </Button>
                    </Col>
                   </Row>
-          </Col>
-        </Row>
+                </Form> */}
+            </Col>
+          </Row>
+        </Container>
       </Container>
-    </Container> 
-    <Footer />
+      <Footer />
     </>
   );
-}
+};
+
+export default Booking;
