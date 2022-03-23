3053cb3f363f92ca193bfdebc2db0ee57b96e1f6
MitchellQ <mitchell.quarrie@outlook.com>
2022-03-23 13:08:35 +1300

    Initial work on Property Card

--- src/components/PropertyView/PropertyCard.js
+++ src/components/PropertyView/PropertyCard.js
@@ -7,7 +7,21 @@ import Map from '../PropertyView/Map'
 const PropertyCard = ({ Property, List }) => {
 
     return (
-        <Map address={Property.Property.address} />
+        <div className='property-card'>
+            <div className='card-left'>
+                <div className='left-top'>
+                    <h3 className='address'>{Property.Property.address}</h3>
+                    <h4 className='price'>{`$${Property.Property.pricePerWeek}/wk`}</h4>
+                </div>
+                <div className='left-middle'></div>
+
+                <Map address={Property.Property.address} />
+
+            </div>
+            <div className='card-right'>
+
+            </div>
+        </div>
     )
 }
 
